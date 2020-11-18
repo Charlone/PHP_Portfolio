db582811c34788abff264e8001a3848a1547d7f6
Charlone <charlone_91@hotmail.com>
2020-11-18 19:48:28 +0100

    added symfony encore pack, yarn to compile bootstrap jquery js and node npm via encore pack, default controller to render landing page and corresponding twig

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+import '../public/assets/scss/style.scss'
+import "../node_modules/bootstrap/scss/bootstrap.scss";
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+const $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/app.js');
