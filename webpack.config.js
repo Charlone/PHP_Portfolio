5b3b47e1638fd2b521882a102ecfd5004b9ef2af
Charlone <charlone_91@hotmail.com>
2020-11-18 21:55:41 +0100

    add images for system, html content for twig and styles

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    // .addEntry('bootstrap', '../public/assets/js/Bootstrap/js//bootstrap.js')
+    //.addEntry('page1', './assets/page1.js')
     //.addEntry('page2', './assets/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
@@ -64,7 +64,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
