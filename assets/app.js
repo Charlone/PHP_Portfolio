24d1bfdffa0773265011a834e33da20dacf16941
Charlone <charlone_91@hotmail.com>
2020-11-29 22:52:44 +0100

    add devepndencies, add imports from node add style change on lower res devices

--- assets/app.js
+++ assets/app.js
@@ -18,5 +18,22 @@ import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 const $ = require('jquery');
 require('../node_modules/@fortawesome/fontawesome-free/js/all');
+require('../node_modules/async/all.js');
+require('../node_modules/jquery/src/jquery.js');
+require('../node_modules/bootstrap/js/src/button.js');
+require('../node_modules/bootstrap/js/src/alert.js');
+require('../node_modules/bootstrap/js/src/carousel.js');
+require('../node_modules/bootstrap/js/src/collapse.js');
+require('../node_modules/bootstrap/js/src/dropdown.js');
+require('../node_modules/bootstrap/js/src/index.js');
+require('../node_modules/bootstrap/js/src/modal.js');
+require('../node_modules/bootstrap/js/src/popover.js');
+require('../node_modules/bootstrap/js/src/scrollspy.js');
+require('../node_modules/bootstrap/js/src/tab.js');
+require('../node_modules/bootstrap/js/src/toast.js');
+require('../node_modules/bootstrap/js/src/tooltip.js');
+require('../node_modules/bootstrap/js/src/util.js');
+require('../node_modules/bootstrap/js/src/tools/sanitizer.js');
+// require('../node_modules/popper/index.js');
 
 console.log('Hello Webpack Encore! Edit me in assets/app.js');
